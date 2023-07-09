c1584b49210bdd74102ffd116d6da1c3f74716b3
Parth Bhattad <bhattadparth20@gmail.com>
2023-07-09 21:30:35 +0530

    photos

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import Swaroop from "../assests/swaroop.jpg"
-import Shreekar from "../assests/EDC SHREEKAR.jpg"
-// import ruchita  from "../assests/ruchita.jpg"
-// import anushray from "../assests/anushray.png"
-// import prithvi from "../assests/prithvi .png"
-// import prathamesh from "../assests/prathamesh.jpg"
-// import mitesh from "../assests/mitesh.jpg"
+import Swaroop from "../assests/swaroop.jpg";
+import Shreekar from "../assests/EDC SHREEKAR.jpg";
+import ruchita from "../assests/ruchita.jpg";
+import anushray from "../assests/anushray.png";
+import prithvi from "../assests/prithvi .png";
+import prathamesh from "../assests/prathamesh.jpg";
+import mitesh from "../assests/mitesh.jpg";
+import Gauri from "../assests/gauri.jpeg";
+import pritam from "../assests/pritam.jpg";
 
 const Teams = () => {
     return (
@@ -16,9 +18,7 @@ const Teams = () => {
                         <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-cyan-400 border-b-4 border-yellow-500 ">
                             Our team
                         </h2>
-                        <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">
-                         
-                        </p>
+                        <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400"></p>
                     </div>
                     <div className="grid gap-8 lg:gap-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                         <div className="text-center text-gray-500 dark:text-gray-400">
@@ -30,178 +30,162 @@ const Teams = () => {
                             <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                                 <a href="www.google.com">Swaroop Bharaskar</a>
                             </h3>
-                            <p>General Secretery</p>
-                           
+                            <p>General Secretary</p>
+                        </div>
+                        <div className="text-center text-gray-500 dark:text-gray-400">
+                            <img
+                                className="mx-auto mb-4 w-36 h-36 rounded-full"
+                                src=""
+                                alt="sharvari Avatar"
+                            />
+                            <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                <a href="www.google.com">Sharvari Karhe </a>
+                            </h3>
+                            <p>Jt. General Secretary</p>
                         </div>
                         <div className="text-center text-gray-500 dark:text-gray-400">
                             <img
                                 className="mx-auto mb-4 w-36 h-36 rounded-full"
                                 src={Shreekar}
-                                alt="Helene Avatar"
+                                alt="shreekar Avatar"
                             />
                             <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                <a href="www.google.com">Swaroop Bharaskar</a>
+                                <a href="www.google.com">Shreekar Kulkarni</a>
                             </h3>
-                            <p>CTO/Co-founder</p>
-                           
+                            <p>Treasurer</p>
                         </div>
                         <div className="text-center text-gray-500 dark:text-gray-400">
                             <img
                                 className="mx-auto mb-4 w-36 h-36 rounded-full"
-                                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png"
-                                alt="Jese Avatar"
+                                src={anushray}
+                                alt="anushray Avatar"
                             />
                             <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                <a href="www.google.com">Jese Leos</a>
+                                <a href="www.google.com">Anushray Pingale</a>
                             </h3>
-                            <p>SEO & Marketing</p>
-                            
+                            <p>Head of sponsorship</p>
                         </div>
                         <div className="text-center text-gray-500 dark:text-gray-400">
                             <img
                                 className="mx-auto mb-4 w-36 h-36 rounded-full"
-                                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/joseph-mcfall.png"
-                                alt="Joseph Avatar"
+                                src={ruchita}
+                                alt="ruchita Avatar"
                             />
                             <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                <a href="www.google.com">Joseph Mcfall</a>
+                                <a href="www.google.com">Ruchita Kagne</a>
                             </h3>
-                            <p>Sales</p>
-                            
+                            <p>Thrust secretary </p>
                         </div>
                         <div className="text-center text-gray-500 dark:text-gray-400">
                             <img
                                 className="mx-auto mb-4 w-36 h-36 rounded-full"
-                                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/sofia-mcguire.png"
-                                alt="Sofia Avatar"
+                                src={mitesh}
+                                alt="mitesh Avatar"
                             />
                             <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                <a href="www.google.com">Lana Byrd</a>
+                                <a href="www.google.com">Mitesh Kulkarni</a>
                             </h3>
-                            <p>Web Designer</p>
-                            
+                            <p>Media Head</p>
                         </div>
                         <div className="text-center text-gray-500 dark:text-gray-400">
                             <img
                                 className="mx-auto mb-4 w-36 h-36 rounded-full"
-                                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/thomas-lean.png"
-                                alt="Leslie Avatar"
+                                src={prathamesh}
+                                alt="prathamesh Avatar"
                             />
                             <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                <a href="www.google.com">Leslie Livingston</a>
+                                <a href="www.google.com">Prathamesh Bhagwat</a>
                             </h3>
-                            <p>Graphic Designer</p>
-                           
+                            <p>Design Head</p>
                         </div>
                         <div className="text-center text-gray-500 dark:text-gray-400">
                             <img
                                 className="mx-auto mb-4 w-36 h-36 rounded-full"
-                                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png"
-                                alt="Michael Avatar"
+                                src=""
+                                alt="Parth Avatar"
                             />
                             <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                <a href="www.google.com">Michael Gough</a>
+                                <a href="www.google.com">Parth Bhattad</a>
                             </h3>
-                            <p>React Developer</p>
-                            
+                            <p>Technical Head</p>
                         </div>
                         <div className="text-center text-gray-500 dark:text-gray-400">
                             <img
                                 className="mx-auto mb-4 w-36 h-36 rounded-full"
-                                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/neil-sims.png"
-                                alt="Neil Avatar"
+                                src={prithvi}
+                                alt="prithvi Avatar"
                             />
                             <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                <a href="www.google.com">Neil Sims</a>
+                                <a href="www.google.com">Prithviraj Patil</a>
                             </h3>
-                            <p>Vue.js Developer</p>
-                           
+                            <p>NEC Head</p>
                         </div>
                         <div className="text-center text-gray-500 dark:text-gray-400">
                             <img
                                 className="mx-auto mb-4 w-36 h-36 rounded-full"
-                                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/neil-sims.png"
-                                alt="Neil Avatar"
+                                src={pritam}
+                                alt="Pritam Avatar"
                             />
                             <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                <a href="www.google.com">Neil Sims</a>
+                                <a href="www.google.com">Pritam Patil</a>
                             </h3>
-                            <p>Vue.js Developer</p>
-                           
+                            <p>Public Relation Head</p>
                         </div>
                         <div className="text-center text-gray-500 dark:text-gray-400">
                             <img
                                 className="mx-auto mb-4 w-36 h-36 rounded-full"
-                                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/neil-sims.png"
-                                alt="Neil Avatar"
+                                src=""
+                                alt="jayesh Avatar"
                             />
                             <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                <a href="www.google.com">Neil Sims</a>
+                                <a href="www.google.com">Jayesh Suryawanshi</a>
                             </h3>
-                            <p>Vue.js Developer</p>
-                           
+                            <p>Startup Executive</p>
                         </div>
                         <div className="text-center text-gray-500 dark:text-gray-400">
                             <img
                                 className="mx-auto mb-4 w-36 h-36 rounded-full"
-                                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/neil-sims.png"
-                                alt="Neil Avatar"
+                                src=""
+                                alt="Rajasee Avatar"
                             />
                             <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                <a href="www.google.com">Neil Sims</a>
+                                <a href="www.google.com">Rajasee Thakre</a>
                             </h3>
-                            <p>Vue.js Developer</p>
-                           
+                            <p>Content Developement Head</p>
                         </div>
                         <div className="text-center text-gray-500 dark:text-gray-400">
                             <img
                                 className="mx-auto mb-4 w-36 h-36 rounded-full"
-                                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/neil-sims.png"
-                                alt="Neil Avatar"
+                                src={Gauri}
+                                alt="Gauri Avatar"
                             />
                             <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                <a href="www.google.com">Neil Sims</a>
+                                <a href="www.google.com">Gauri Jadhav</a>
                             </h3>
-                            <p>Vue.js Developer</p>
-                           
+                            <p>Asthetics Head</p>
                         </div>
                         <div className="text-center text-gray-500 dark:text-gray-400">
                             <img
                                 className="mx-auto mb-4 w-36 h-36 rounded-full"
-                                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/neil-sims.png"
-                                alt="Neil Avatar"
+                                src=""
+                                alt="shreya1 Avatar"
                             />
                             <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                <a href="www.google.com">Neil Sims</a>
+                                <a href="www.google.com">Shreya V. Patil</a>
                             </h3>
-                            <p>Vue.js Developer</p>
-                           
+                            <p>Documentation Head</p>
                         </div>
                         <div className="text-center text-gray-500 dark:text-gray-400">
                             <img
                                 className="mx-auto mb-4 w-36 h-36 rounded-full"
-                                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/neil-sims.png"
-                                alt="Neil Avatar"
+                                src=""
+                                alt="Shreya 2 avatar"
                             />
                             <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                <a href="www.google.com">Neil Sims</a>
+                                <a href="www.google.com">Shreya S. Patil</a>
                             </h3>
-                            <p>Vue.js Developer</p>
-                           
+                            <p>Documentation Co-Head</p>
                         </div>
-                        <div className="text-center text-gray-500 dark:text-gray-400">
-                            <img
-                                className="mx-auto mb-4 w-36 h-36 rounded-full"
-                                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/neil-sims.png"
-                                alt="Neil Avatar"
-                            />
-                            <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                <a href="www.google.com">Neil Sims</a>
-                            </h3>
-                            <p>Vue.js Developer</p>
-                           
-                        </div>
-                        
                     </div>
                 </div>
             </section>
