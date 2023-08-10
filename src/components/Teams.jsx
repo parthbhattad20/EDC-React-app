05f8d8da3ff02d5f016080b77ce089cd8352b72c
Parth Bhattad <bhattadparth20@gmail.com>
2023-08-10 19:15:11 +0530

    .

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -88,7 +88,7 @@ const Teams = () => {
         </div>
         <div className="grid gap-12 items-center md:grid-cols-3">
           <div className="space-y-4 text-center mb-3">
-          <Slide triggerOnce={true} direction="right">
+          <Slide triggerOnce={true} >
             <img
               className="w-64 h-64 mx-auto object-cover rounded-xl md:w-40 md:h-40 lg:w-64 lg:h-64"
               src={Swaroop}
@@ -106,7 +106,7 @@ const Teams = () => {
             </div>
           </div>
           <div className="space-y-4 text-center mb-3">
-          <Slide triggerOnce={true} >
+          <Slide triggerOnce={true} direction="right" >
             <img
               className="w-64 h-64 mx-auto object-cover rounded-xl md:w-48 md:h-64 lg:w-64 lg:h-64"
               src={Swaroop}
@@ -124,7 +124,68 @@ const Teams = () => {
             </div>
           </div>
           <div className="space-y-4 text-center mb-3">
+          <Slide triggerOnce={true} >
+            <img
+              className="w-64 h-64 mx-auto object-cover rounded-xl md:w-40 md:h-40 lg:w-64 lg:h-64"
+              src={Swaroop}
+              alt="woman"
+              loading="lazy"
+              width="1000"
+              height="667"
+            />
+            </Slide>
+            <div>
+              <Fade triggerOnce={true}>
+              <h4 className="text-2xl">Anabelle Doe</h4>
+              <span className="block text-sm text-gray-500">Chief Operations Officer</span>
+              </Fade>
+            </div>
+
+            
+
+            
+            
+          </div>
+        </div>
+        <div className="grid gap-12 items-center md:grid-cols-3">
+          <div className="space-y-4 text-center mb-3">
           <Slide triggerOnce={true} direction="right">
+            <img
+              className="w-64 h-64 mx-auto object-cover rounded-xl md:w-40 md:h-40 lg:w-64 lg:h-64"
+              src={Swaroop}
+              alt="woman"
+              loading="lazy"
+              width="640"
+              height="805"
+            />
+            </Slide>
+            <div>
+            <Fade triggerOnce={true}>
+              <h4 className="text-2xl text-white">Swaroop Bharaskar</h4>
+              <span className="block text-sm text-gray-500">General Secretary</span>
+              </Fade>
+            </div>
+          </div>
+          <div className="space-y-4 text-center mb-3">
+          <Slide triggerOnce={true} direction="right">
+            <img
+              className="w-64 h-64 mx-auto object-cover rounded-xl md:w-48 md:h-64 lg:w-64 lg:h-64"
+              src={Swaroop}
+              alt="man"
+              loading="lazy"
+              width="1000"
+              height="667"
+            />
+            </Slide>
+            <div>
+            <Fade triggerOnce={true}>
+              <h4 className="text-2xl">Jonathan Doe</h4>
+              <span className="block text-sm text-gray-500">Chief Technical Officer</span>
+              </Fade>
+            </div>
+          </div>
+          <div className="space-y-4 text-center mb-3">
+          <Slide triggerOnce={true} >
             <img
               className="w-64 h-64 mx-auto object-cover rounded-xl md:w-40 md:h-40 lg:w-64 lg:h-64"
               src={Swaroop}
@@ -210,7 +271,7 @@ const Teams = () => {
         </div>
         <div className="grid gap-12 items-center md:grid-cols-3">
           <div className="space-y-4 text-center mb-3">
-          <Slide triggerOnce={true} direction="right">
+          <Slide triggerOnce={true}>
             <img
               className="w-64 h-64 mx-auto object-cover rounded-xl md:w-40 md:h-40 lg:w-64 lg:h-64"
               src={Swaroop}
@@ -228,68 +289,7 @@ const Teams = () => {
             </div>
           </div>
           <div className="space-y-4 text-center mb-3">
-          <Slide triggerOnce={true} >
-            <img
-              className="w-64 h-64 mx-auto object-cover rounded-xl md:w-48 md:h-64 lg:w-64 lg:h-64"
-              src={Swaroop}
-              alt="man"
-              loading="lazy"
-              width="1000"
-              height="667"
-            />
-            </Slide>
-            <div>
-            <Fade triggerOnce={true}>
-              <h4 className="text-2xl">Jonathan Doe</h4>
-              <span className="block text-sm text-gray-500">Chief Technical Officer</span>
-              </Fade>
-            </div>
-          </div>
-          <div className="space-y-4 text-center mb-3">
-          <Slide triggerOnce={true} direction="right">
-            <img
-              className="w-64 h-64 mx-auto object-cover rounded-xl md:w-40 md:h-40 lg:w-64 lg:h-64"
-              src={Swaroop}
-              alt="woman"
-              loading="lazy"
-              width="1000"
-              height="667"
-            />
-            </Slide>
-            <div>
-              <Fade triggerOnce={true}>
-              <h4 className="text-2xl">Anabelle Doe</h4>
-              <span className="block text-sm text-gray-500">Chief Operations Officer</span>
-              </Fade>
-            </div>
-
-            
-
-            
-            
-          </div>
-        </div>
-        <div className="grid gap-12 items-center md:grid-cols-3">
-          <div className="space-y-4 text-center mb-3">
-          <Slide triggerOnce={true} direction="right">
-            <img
-              className="w-64 h-64 mx-auto object-cover rounded-xl md:w-40 md:h-40 lg:w-64 lg:h-64"
-              src={Swaroop}
-              alt="woman"
-              loading="lazy"
-              width="640"
-              height="805"
-            />
-            </Slide>
-            <div>
-            <Fade triggerOnce={true}>
-              <h4 className="text-2xl text-white">Swaroop Bharaskar</h4>
-              <span className="block text-sm text-gray-500">General Secretary</span>
-              </Fade>
-            </div>
-          </div>
-          <div className="space-y-4 text-center mb-3">
-          <Slide triggerOnce={true} >
+          <Slide triggerOnce={true} direction="right" >
             <img
               className="w-64 h-64 mx-auto object-cover rounded-xl md:w-48 md:h-64 lg:w-64 lg:h-64"
               src={Swaroop}
@@ -307,7 +307,7 @@ const Teams = () => {
             </div>
           </div>
           <div className="space-y-4 text-center">
-          <Slide triggerOnce={true} direction="right">
+          <Slide triggerOnce={true} >
             <img
               className="w-64 h-64 mx-auto object-cover rounded-xl md:w-40 md:h-40 lg:w-64 lg:h-64"
               src={Swaroop}
