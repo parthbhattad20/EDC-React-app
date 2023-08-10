07fb630278c706f584df329c1ad5bd990e7f056a
Parth Bhattad <bhattadparth20@gmail.com>
2023-08-10 19:19:47 +0530

    .

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
           </h2>
      
           
-          <p className="text-white py-4 max-w-screen-lg w-full lg:text-2xl  ">
+          <p className="text-white text-justify py-4 max-w-screen-lg w-full lg:text-2xl  ">
            
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing  
 
