981b225f737ee0c7a0f5faaf694f247d17fc9b88
parthbhattad20 <bhattadparth20@gmail.com>
2023-08-17 08:39:42 +0100

    navbbar changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-scroll';
 
 const NavBar = () => {
   return (
-    <nav className="bg-[#474B4F] p-4 fixed w-full top-0 z-10">
+    <nav className="bg-black p-4 fixed w-full top-0 z-10">
       <div className="container mx-auto flex justify-center">
         <ul className="flex space-x-6">
           <li className="nav-item">
