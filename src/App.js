8a2a2d862fadb586c62bfe61e9af087bd3ab09a4
parthbhattad20 <bhattadparth20@gmail.com>
2023-08-17 11:40:50 +0100

    done

--- src/App.js
+++ src/App.js
@@ -5,22 +5,62 @@ import Navbar from "./components/Navbar";
 import SocialLinks from "./components/SocialLinks";
 import Teams from "./components/Teams";
 import Footer from "./components/Footer";
+import PacmanLoader from "react-spinners/PacmanLoader";
 
 import Whatwedo from "./components/Whatwedo";
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-       <div><Navbar/></div>
-       <div><Home/></div>
-       <div ><About/></div>
-       <div ><Whatwedo/></div>
-       <div><Teams/></div>
-       <div ><Contact/></div>
-       <div><Footer/></div>
-       <div ><SocialLinks/></div>
-    </div>
-  );
+    const [loading, setloading] = useState(false);
+
+    useEffect(() => {
+        setloading(true);
+        setTimeout(() => {
+            setloading(false);
+        }, 3000);
+    }, []);
+
+    return (
+        <div className="App">
+            {loading ? (
+                <PacmanLoader
+                    className="justify-center"
+                    color="#36d7b7"
+                    loading={loading}
+                    size={70}
+                    aria-label="Loading Spinner"
+                    data-testid="loader"
+                />
+            ) : (
+                <div>
+                    <div className="header">
+                        <Navbar />
+                    </div>
+                    <div>
+                        <Home />
+                    </div>
+                    <div>
+                        <About />
+                    </div>
+                    <div>
+                        <Whatwedo />
+                    </div>
+                    <div>
+                        <Teams />
+                    </div>
+                    <div>
+                        <Contact />
+                    </div>
+                    <div>
+                        <Footer />
+                    </div>
+                    <div>
+                        <SocialLinks />
+                    </div>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
