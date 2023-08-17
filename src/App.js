f5c5ec14f18e42e62501bc2d2df7064c38fb58e9
parthbhattad20 <bhattadparth20@gmail.com>
2023-08-17 19:26:38 +0100

    ad

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
     return (
         <div className="App">
             {loading ? (
-                     <div className="h-[700px] bg-black flex items-center justify-center ">
+                     <div className="h-[700px] bg-black flex items-center justify-center lg:h-[900px]">
                 <img
                     src={video}
                     className=""
