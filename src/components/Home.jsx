cf6cf882a53627040596bbe5c1552b58dc271bc5
parthbhattad20 <bhattadparth20@gmail.com>
2023-08-19 11:22:17 +0100

    hehe

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { Fade } from "react-awesome-reveal";
-import bgimg from "../assests/bgimg3.jpg"
+import bgimg from "../assests/bgimg4.jpg"
 
 const Home = () => {
     return (
@@ -12,25 +12,15 @@ const Home = () => {
                 backgroundImage:`url(${bgimg})`,
             }}
         >
-            <div className="max-w-screen-lg mx-auto flex flex-col h-full px-4 md:flex-row">
-                <div className="flex flex-col mt-20 justify-start h-[200px]  justify-end w-full align-baseline">
+           <div className="max-w-screen-lg mx-auto flex flex-col h-full px-4 md:flex-row">
+                <div className=" items-center flex flex-col mt-20 justify-start h-[200px]  justify-end w-full align-baseline">
                     <Fade triggerOnce={true}>
                         <h2 className="text-4xl sm:text-7xl font-bold text-[#66F6F1] ">
-                            Welcome to E-Cell
-                        </h2>
+                          We Aspire To Inspire
 
-                        {/* <p className="text-[#C5C6C7] text-justify py-4 max-w-screen-lg w-full lg:text-2xl  ">
-                            Enter the dynamic realm of E-Cell PVG, where
-                            innovation meets entrepreneurship. We fuel the
-                            spirit of inventive minds through workshops, events,
-                            and a tight-knit community. From refining business
-                            acumen to incubating startups, we're the launchpad
-                            for visionary leaders. Step into a world where ideas
-                            evolve into impactful ventures, shaping the
-                            entrepreneurial landscape. Let's collaborate to
-                            redefine the future of success.
-                        </p> */}
-                    </Fade>
+                                         </h2>
+
+                        </Fade>
 
                     <div>
                         
